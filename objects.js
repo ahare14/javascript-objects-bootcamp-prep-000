99db74cdb4f25deef9f15ad959bed2ae93c516e8
Alexander Hare <alexanderhare14@gmail.com>
2018-10-29 13:23:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@
 var playlist = {'Lady Gaga' : 'Poker Face'};
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist['artistName'] = 'songTitle';
+  playlist.Beatles = 'Yellow Submarine';
   return playlist
 }
